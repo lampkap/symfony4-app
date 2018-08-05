67fea89a89038c3f80c437c5bda79bdd00cb43c7
diemleyssen <diem@novation.be>
2018-08-05 16:11:21 +0200

    added command to import members and added a command to generate a csv file

---
+++ src/Command/GenerateCsvCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class GenerateCsvCommand extends Command
+{
+    protected static $defaultName = 'generate:csv';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $file = $input->getArgument('arg1');
+
+        if ($file) {
+            $info = pathinfo($file);
+            // check if file is op type csv
+            if($info['extension'] === 'csv') {
+                // everything checks out so let's import
+                $this->generateCsv($file, $io);
+            } else {
+                $io->error('Het bestand moet van het type CSV zijn');
+            }
+        } else {
+            $io->error('Geef een bestandspad mee als argument');
+        }
+    }
+
+    protected function generateCsv($file, SymfonyStyle $io)
+    {
+        for($i = 0; $i <= 1000; $i++) {
+            $number = $this->generateNumber();
+            $date = $this->generateDate();
+
+            $record = $date.';'.$number;
+
+            file_put_contents($file, $record . PHP_EOL, FILE_APPEND);
+        }
+    }
+
+
+    protected function generateNumber()
+    {
+        $numbers = range(1000, 30000);
+        $i = rand(0, count($numbers) - 1);
+
+        return $numbers[$i];
+    }
+
+    protected function generateDate()
+    {
+        $days = range(1, 28);
+        $months = range(1, 12);
+        $years = range(1930, 2000);
+
+        $daysI = rand(0, count($days) - 1);
+        $monthsI = rand(0, count($months) - 1);
+        $yearsI = rand(0, count($years) - 1);
+
+        return $days[$daysI] . '/' . $months[$monthsI] . '/' . $years[$yearsI];
+    }
+}
---
+++ src/Command/ImportMembersCommand.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Command;
+
+use App\Controller\GiftController;
+use App\Entity\Member;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ImportMembersCommand extends ContainerAwareCommand
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'import:members';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $file = $input->getArgument('arg1');
+
+        if ($file) {
+            // check if file exists
+            if(file_exists($file)) {
+                $info = pathinfo($file);
+                // check if file is op type csv
+                if($info['extension'] === 'csv') {
+                    // everything checks out so let's import
+                    $this->importMembers($file, $io);
+                } else {
+                    $io->error('Het bestand moet van het type CSV zijn');
+                }
+            } else {
+                $io->error(sprintf('Er werd geen bestand %s gevonden', $file));
+            }
+        } else {
+            $io->error('Geef een bestandspad mee als argument');
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+    }
+
+    /**
+     * @param $file
+     * @param SymfonyStyle $io
+     */
+    protected function importMembers($file, SymfonyStyle $io)
+    {
+        // reformat the values of the csv to an array
+        $members = $this->parseCsv($file);
+        if(!$members) {
+            $io->error('Het csv bestand kon niet worden verwerkt of heeft geen inhoud');
+            exit;
+        }
+        // set import status values
+        $created = 0;
+        $updated = 0;
+        $skipped = 0;
+
+        // loop through all csv records
+        foreach($members as $memberValues) {
+            // check if the date is configurable
+            $date = GiftController::formatBirthdate($memberValues[0]);
+            if(!$date) {
+                // if not, we stop the import
+                $io->error('De datum ' . $memberValues[0] . ' is niet correct. Het importeren is onderbroken');
+                exit;
+            } else {
+                $em = $this->getContainer()->get('doctrine')->getEntityManager();
+                // check if a member exists with a specific member number
+                $member = $em->getRepository(Member::class)->findBy(array('number' => $memberValues[1]));
+                // if the member does not exist, we create one
+                if(empty($member)) {
+
+                    $this->createMember($date, $memberValues[1]);
+
+                    $io->writeln('Lid ' . $memberValues[1] . ', geboren op ' . $date->format('d-m-Y') . ' geïmporteerd');
+                    $created++;
+                // else we check if we need to update the member
+                } else {
+                    // check if the birthdate has changed of a member
+                    $hasUpdated = $this->updateMember($member, $date);
+                    // if it has, he will be updated
+                    if($hasUpdated) {
+                        $io->writeln('Lid ' . $memberValues[1] . ', geboren op ' . $date->format('d-m-Y') . ' geüpdate');
+                        $updated++;
+                    // else he will be skipped
+                    } else {
+                        $io->writeln('Lid ' . $memberValues[1] . ' is niet gewijzigd.');
+                        $skipped++;
+                    }
+
+                }
+            }
+        }
+
+        $io->success('Import doorgevoerd!');
+        $io->table(array('Aangemaakt', 'Aangepast', 'Overgeslagen'), array(array($created, $updated, $skipped)));
+
+    }
+
+    /**
+     * @param $file
+     * @return array|bool
+     */
+    protected function parseCsv($file)
+    {
+        $data = array();
+        if (($handle = fopen($file, 'r')) !== FALSE) {
+            while (($row = fgetcsv($handle, 0, ';')) !== FALSE) {
+                $data[] = $row;
+            }
+            fclose($handle);
+        }
+
+        return (empty($data)) ? false : $data;
+    }
+
+    /**
+     * @param $date
+     * @param $number
+     */
+    protected function createMember($date, $number)
+    {
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+        $member = new Member();
+
+        $member->setBirthdate($date);
+        $member->setNumber($number);
+
+        $em->persist($member);
+        $em->flush();
+    }
+
+    /**
+     * @param $member
+     * @param $date
+     * @return bool
+     */
+    protected function updateMember($member, $date)
+    {
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+        $member = reset($member);
+
+        if($member->getBirthdate() != $date) {
+            $member->setBirthdate($date);
+
+            $em->persist($member);
+            $em->flush();
+
+            // return true to say the member could and has been updated
+            return true;
+
+        } else {
+            // return false to say the member is not updated since the values did not change
+            return false;
+        }
+    }
+}
