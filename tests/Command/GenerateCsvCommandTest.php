8005d5a2717c4ea85d49610471bb130f3c278f0e
diemleyssen <diem@novation.be>
2018-08-07 21:33:51 +0200

    added testing for commands

---
+++ tests/Command/GenerateCsvCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests\Command;
+
+use App\Command\GenerateCsvCommand;
+use App\Controller\MemberController;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+class GenerateCsvCommandTest extends TestCase
+{
+    protected static function getMethod($name)
+    {
+        $class = new ReflectionClass('App\Command\GenerateCsvCommand');
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+
+    public function testIfNumberIsGenerated()
+    {
+        $command = new GenerateCsvCommand();
+        $method = self::getMethod('generateNumber');
+
+        $result = $method->invokeArgs($command, array());
+
+        $this->assertInternalType('integer', $result);
+        $this->assertGreaterThanOrEqual(1000, $result);
+        $this->assertLessThanOrEqual(30000, $result);
+    }
+
+    public function testIfValidDateIsGenerated()
+    {
+        $command = new GenerateCsvCommand();
+        $method = self::getMethod('generateDate');
+
+        $result = $method->invokeArgs($command, array());
+
+        $this->assertNotFalse(MemberController::formatBirthdate($result));
+    }
+
+    public function testIfCsvFileIsCreated()
+    {
+        $csv = '/Users/diem/Desktop/test.csv';
+
+        $command = new GenerateCsvCommand();
+        $method = self::getMethod('generateCsv');
+
+        $method->invokeArgs($command, array($csv));
+
+        $this->assertFileExists($csv);
+
+        unlink($csv);
+    }
+}
